721fab69f01cb7a135d84c44a243abb59666cb1c
stargirl369 <robyn.wade369@gmail.com>
2025-12-22 06:26:47 -0600

    Create Home component in index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>It works.</h1>
+      <p>This page is alive.</p>
+    </div>
+  );
+}
