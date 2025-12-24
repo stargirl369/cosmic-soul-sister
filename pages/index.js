9e9c8e75aaf4bff92702c5e6279cd5957fd3ee70
stargirl369 <robyn.wade369@gmail.com>
2025-12-23 21:05:10 -0600

    Simplify Home component content
    Removed previous content and updated the message.

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
 export default function Home() {
   return (
-    <div style={{
-      minHeight: '100vh',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      background: '#0b0f19',
-      color: '#eaeaf0',
-      fontFamily: 'system-ui'
-    }}>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Cosmic Soul Sister 🌙</h1>
-        <p>This page is alive and listening.</p>
-      </div>
+    <div>
+      <h1>It works.</h1>
     </div>
   );
 }
